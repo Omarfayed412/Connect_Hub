12e1e5ed360edb72c75887b2c55846caa1b0dd52
Omar Fayed <omarfayed381@gmail.com>
2024-12-10 01:57:38 +0200

    added group notifications and friend requests notifications

---
+++ Connect_Hub/src/Backend/Notifications/FriendRequestNotification.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Backend.Notifications;
+
+import Backend.User.User;
+
+/**
+ *
+ * @author Omar Fayed
+ */
+
+public class friendRequestNotification {
+    //profile of sender/acceptor
+    private User user = null;
+
+    public friendRequestNotification(User user) {
+        this.user = user;
+    }
+    
+    //returns profile of sender/acceptor
+    public User getContent() {
+        return this.user;
+    }
+    
+    //pass the profile of sender
+    public void setContent(User user) {
+        this.user = user;
+    }
+    
+    //returns a string containing the info of the notification
+    public String toStringAccepted() {
+        return this.user.getUsername() + "has accepted your friend request.\n";
+    }
+    
+    public String toStringRecieved() {
+        return this.user.getUsername() + "has sent you a friend request.\n";
+    }
+}
